e620cdf1d94bef3fd274fce4653fceca6b56a14f
shinkxu <shinkxu@163.com>
2019-12-26 10:51:37 +0800

    first commit

---
+++ src/main/java/com/wintec/identification_web/IdentificationWebApplication.java
@@ -0,0 +1,13 @@
+package com.wintec.identification_web;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IdentificationWebApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IdentificationWebApplication.class, args);
+	}
+
+}
